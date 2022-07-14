cb972d50f205bf55af073a8029459e2e1bc0bb49
Vadim Vladimirov <kurtonchik@gmail.com>
2022-07-14 21:02:23 +0300

    fix add controller

--- controllers/diary/addProduct.js
+++ controllers/diary/addProduct.js
@@ -1,20 +1,12 @@
 const { SpecificDay } = require("../../models");
-const { BadRequest } = require("http-errors");
 const { convertKcal } = require("../../utils/");
 
 const addProduct = async (req, res) => {
   const { _id } = req.user;
   const { title, weight } = req.body;
-  console.log(title);
-
-  const verifyProduct = await SpecificDay.findOne({ title });
-
-  if (verifyProduct) {
-    throw new BadRequest("Product already add");
-  }
 
   const productKcal = await convertKcal(title, weight);
-  console.log(productKcal);
+
   const productToAdd = await SpecificDay.create({
     ...req.body,
     kcal: productKcal,

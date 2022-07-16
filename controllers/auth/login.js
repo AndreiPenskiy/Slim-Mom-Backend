8f2eabfb44a9920a6bdab0a7f0a208a30d2c9b5a
Vitalii-Krat <kratvetal@ukr.net>
2022-07-17 00:08:51 +0300

    fix signup token

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -16,7 +16,7 @@ const login = async (req, res) => {
     id: user._id,
   };
 
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "10h" });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "12h" });
   await User.findByIdAndUpdate(user._id, { token, verify: true });
 
   res.json({
--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,5 +1,8 @@
 const { Conflict } = require("http-errors");
 const { User } = require("../../models");
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = process.env;
 
 const signup = async (req, res) => {
   const { name, email, password } = req.body;
@@ -8,14 +11,24 @@ const signup = async (req, res) => {
     throw new Conflict(`User with email ${email} already exist`);
   }
 
-  const newUser = new User({ name, email });
+  const newUser = await new User({ name, email });
   newUser.setPassword(password);
-  newUser.save();
+  const newUserCreated = await newUser.save();
+  const payload = {
+    id: newUserCreated._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "12h" });
+
+  await User.findByIdAndUpdate(newUserCreated._id, {
+    token,
+    verify: true,
+  });
 
   res.status(201).json({
     status: "success",
     code: 201,
     data: {
+      token,
       user: {
         name,
         email,

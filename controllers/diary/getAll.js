8e915753e5a7bfa64be3cfd8a7c00e0f0dbb3dfe
Vadim Vladimirov <kurtonchik@gmail.com>
2022-07-17 22:01:08 +0300

    change ctrl getAll, getCurrent

--- controllers/diary/getAll.js
+++ controllers/diary/getAll.js
@@ -2,7 +2,7 @@ const { SpecificDay } = require("../../models");
 
 const getAll = async (req, res) => {
   const { _id } = req.user;
-  const { date } = req.params;
+  const { date } = req.query;
   const products = await SpecificDay.find({ owner: _id, date });
   res.json({
     status: "success",
